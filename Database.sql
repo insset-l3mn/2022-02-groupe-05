01c93417b5eb43b2494f75f563a4660b356afca0
augustinde <augustin.1201@outlook.fr>
2022-02-22 15:41:03 +0100

    Mcd version 1.0

--- Database.sql
+++ Database.sql
@@ -4,12 +4,12 @@ CREATE TABLE Utilisateur(
    nom VARCHAR(50) ,
    email VARCHAR(50) ,
    motdepasse VARCHAR(50) ,
+   groupe VARCHAR(50)  NOT NULL,
    PRIMARY KEY(Id_Utilisateur)
 );
 
 CREATE TABLE Formateur(
    Id_Formateur INT AUTO_INCREMENT,
-   groupe VARCHAR(50)  NOT NULL,
    Id_Utilisateur INT NOT NULL,
    PRIMARY KEY(Id_Formateur),
    FOREIGN KEY(Id_Utilisateur) REFERENCES Utilisateur(Id_Utilisateur)
@@ -22,22 +22,16 @@ CREATE TABLE Graphe(
 
 CREATE TABLE Questionnaire(
    Id_Questionnaire INT AUTO_INCREMENT,
+   type VARCHAR(50)  NOT NULL,
    Id_Formateur INT NOT NULL,
    PRIMARY KEY(Id_Questionnaire),
    FOREIGN KEY(Id_Formateur) REFERENCES Formateur(Id_Formateur)
 );
 
-CREATE TABLE Question(
-   Id_Question INT AUTO_INCREMENT,
-   niveau INT NOT NULL,
-   difficulte INT NOT NULL,
-   domaine VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(Id_Question)
-);
-
 CREATE TABLE Competence(
    Id_Competence INT AUTO_INCREMENT,
-   nom VARCHAR(50)  NOT NULL,
+   nom VARCHAR(50) ,
+   poids INT NOT NULL,
    Id_Formateur INT NOT NULL,
    PRIMARY KEY(Id_Competence),
    FOREIGN KEY(Id_Formateur) REFERENCES Formateur(Id_Formateur)
@@ -45,12 +39,45 @@ CREATE TABLE Competence(
 
 CREATE TABLE Formation(
    Id_Formation INT AUTO_INCREMENT,
+   nom VARCHAR(50)  NOT NULL,
    PRIMARY KEY(Id_Formation)
+);
+
+CREATE TABLE Domaine(
+   Id_Domaine INT AUTO_INCREMENT,
+   libelle VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(Id_Domaine)
+);
+
+CREATE TABLE Question(
+   Id_Question INT AUTO_INCREMENT,
+   niveau INT NOT NULL,
+   difficulte INT NOT NULL,
+   contenu VARCHAR(50)  NOT NULL,
+   Id_Competence INT NOT NULL,
+   Id_Domaine INT NOT NULL,
+   PRIMARY KEY(Id_Question),
+   FOREIGN KEY(Id_Competence) REFERENCES Competence(Id_Competence),
+   FOREIGN KEY(Id_Domaine) REFERENCES Domaine(Id_Domaine)
 );
 
 CREATE TABLE Parcours(
    Id_Parcours INT AUTO_INCREMENT,
-   PRIMARY KEY(Id_Parcours)
+   nom VARCHAR(50)  NOT NULL,
+   Id_Formateur INT NOT NULL,
+   Id_Domaine INT NOT NULL,
+   PRIMARY KEY(Id_Parcours),
+   FOREIGN KEY(Id_Formateur) REFERENCES Formateur(Id_Formateur),
+   FOREIGN KEY(Id_Domaine) REFERENCES Domaine(Id_Domaine)
+);
+
+CREATE TABLE Proposition(
+   Id_Proposition INT AUTO_INCREMENT,
+   libelle VARCHAR(50)  NOT NULL,
+   etat BOOLEAN NOT NULL,
+   Id_Question INT NOT NULL,
+   PRIMARY KEY(Id_Proposition),
+   FOREIGN KEY(Id_Question) REFERENCES Question(Id_Question)
 );
 
 CREATE TABLE Apprenant(
@@ -78,7 +105,7 @@ CREATE TABLE Questionnaire_posseder_question(
    FOREIGN KEY(Id_Question) REFERENCES Question(Id_Question)
 );
 
-CREATE TABLE Formateur_gérer_question(
+CREATE TABLE Formateur_gerer_question(
    Id_Formateur INT,
    Id_Question INT,
    PRIMARY KEY(Id_Formateur, Id_Question),
@@ -109,4 +136,20 @@ CREATE TABLE Parcours_avoir_formation(
    PRIMARY KEY(Id_Formation, Id_Parcours),
    FOREIGN KEY(Id_Formation) REFERENCES Formation(Id_Formation),
    FOREIGN KEY(Id_Parcours) REFERENCES Parcours(Id_Parcours)
+);
+
+CREATE TABLE Prerequis_de_competence(
+   Id_Competence INT,
+   Id_Competence_1 INT,
+   PRIMARY KEY(Id_Competence, Id_Competence_1),
+   FOREIGN KEY(Id_Competence) REFERENCES Competence(Id_Competence),
+   FOREIGN KEY(Id_Competence_1) REFERENCES Competence(Id_Competence)
+);
+
+CREATE TABLE Composer_de_competence(
+   Id_Graphe INT,
+   Id_Competence INT,
+   PRIMARY KEY(Id_Graphe, Id_Competence),
+   FOREIGN KEY(Id_Graphe) REFERENCES Graphe(Id_Graphe),
+   FOREIGN KEY(Id_Competence) REFERENCES Competence(Id_Competence)
 );
