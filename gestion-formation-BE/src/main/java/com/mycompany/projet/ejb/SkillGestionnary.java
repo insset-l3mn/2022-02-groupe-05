4212e46afb6d2f4d934c2c382e95a328dcfb6e1b
Valentincp80190 <75424293+Valentincp80190@users.noreply.github.com>
2022-03-03 11:30:42 +0100

    Add CRUD to GfSkill.

--- gestion-formation-BE/src/main/java/com/mycompany/projet/ejb/SkillGestionnary.java
+++ gestion-formation-BE/src/main/java/com/mycompany/projet/ejb/SkillGestionnary.java
@@ -39,25 +39,63 @@ public class SkillGestionnary {
     public void createSkill(GfSkill skill) {
         em.persist(skill);
     }
-    
+
     public GfSkill requestSkill(String NAME) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("userPU");
         EntityManager em1 = emf.createEntityManager();
         Query query = em1.createQuery("SELECT s FROM GfSkill s WHERE s.name = :name").setParameter("name", NAME);
-        
-        if(!query.getResultList().isEmpty()){
+
+        if (!query.getResultList().isEmpty()) {
             return (GfSkill) query.getResultList().get(0);
         }
-        
+
         return null;
     }
-    
+
     public Boolean existSkill(String name) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("userPU");
         EntityManager em1 = emf.createEntityManager();
         Query query = em1.createQuery("SELECT s FROM GfSkill s WHERE s.name = '" + name + "'");
-        
-        if(query.getResultList().isEmpty()) return false;
+
+        if (query.getResultList().isEmpty()) {
+            return false;
+        }
         return true;
+    }
+
+    public Boolean existSkill(int id) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("userPU");
+        EntityManager em1 = emf.createEntityManager();
+        Query query = em1.createQuery("SELECT s FROM GfSkill s WHERE s.idSkill = '" + id + "'");
+
+        if (query.getResultList().isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean removeSkill(int id) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("userPU");
+        EntityManager em1 = emf.createEntityManager();
+        
+        try{
+            em1.createQuery("DELETE FROM GfSkill WHERE idSkill=" + id).executeUpdate();
+            return true;
+        }catch(Exception e)
+        {
+            return false;
+        }
+    }
+    
+    public Boolean updateSkill(int ID, String NAME, String WEIGHT) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("userPU");
+        EntityManager em1 = emf.createEntityManager();
+        try{
+            em1.createQuery("UPDATE GfSkill s SET s.name='" + NAME + "', s.weight='"+ WEIGHT + "'").executeUpdate();
+            return true;
+        }catch(Exception e)
+        {
+            return false;
+        }
     }
 }
--- gestion-formation-BE/src/main/java/com/mycompany/projet/resources/SkillResource.java
+++ gestion-formation-BE/src/main/java/com/mycompany/projet/resources/SkillResource.java
@@ -14,7 +14,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;  
+import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -27,25 +27,68 @@ public class SkillResource {
     private SkillGestionnary skillGestionnary;
     @EJB
     private UserGestionnary userGestionnary;
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/add/{name}/{weight}/{id}")
-    public Message testValue(@PathParam("name") String name, @PathParam("weight") String weight, @PathParam("id") int id) {
+    public Message addSkill(@PathParam("name") String name, @PathParam("weight") String weight, @PathParam("id") int id) {
         if (name != null && weight != null) {
-            if(userGestionnary.isFormer(id)){
+            if (userGestionnary.isFormer(id)) {
                 User user = userGestionnary.requestUser(id);
-                if(!skillGestionnary.existSkill(name)){
+                if (!skillGestionnary.existSkill(name)) {
                     skillGestionnary.createSkill(new GfSkill(user, name, weight));
-                    return new Message("success","La compétance à bien été ajoutée.");
-                }else{
-                    return new Message("error","La compétance existe déjà");
+                    return new Message("success", "La compétance a bien été ajoutée.");
+                } else {
+                    return new Message("error", "La compétance existe déjà");
                 }
-            }else{
-                return new Message("error","Vous n'êtes pas autorisé à effectuer cette action.");
+            } else {
+                return new Message("error", "Vous n'êtes pas autorisé à effectuer cette action.");
             }
-        }else{
+        } else {
             return new Message("error", "Une erreur est survenue lors de l'ajout d'une nouvelle compétance.");
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/remove/{idSkill}/{idUser}")
+    public Message removeSkill(@PathParam("idSkill") int idSkill, @PathParam("idUser") int idUser) {
+        if (userGestionnary.isFormer(idUser)) {
+            if (skillGestionnary.existSkill(idSkill)) {
+                if (skillGestionnary.removeSkill(idSkill)) {
+                    return new Message("success", "La compétance a bien été supprimée.");
+                } else {
+                    return new Message("error", "Une erreur est survenue lors de la suppression d'une compétance.");
+                }
+            } else {
+                return new Message("error", "La compétance n'existe pas.");
+            }
+        } else {
+            return new Message("error", "Vous n'êtes pas autorisé à effectuer cette action.");
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/update/{idSkill}/{name}/{weight}/{idUser}")
+    public Message updateSkill(@PathParam("idSkill") int idSkill, @PathParam("name") String name, @PathParam("weight") String weight, @PathParam("idUser") int idUser) {
+
+        if (name != null && weight != null) {
+            if (userGestionnary.isFormer(idUser)) {
+                if (skillGestionnary.existSkill(idSkill)) {
+                    if (skillGestionnary.updateSkill(idSkill, name, weight)) {
+                        return new Message("success", "La compétance a bien été mise à jour.");
+                    } else {
+                        return new Message("error", "Une erreur est survenue lors de la mise à jour de la compétance.");
+                    }
+                } else {
+                    return new Message("error", "La compétance n'existe pas.");
+                }
+            } else {
+                return new Message("error", "Vous n'êtes pas autorisé à effectuer cette action.");
+            }
+        } else {
+            return new Message("error", "Une erreur est survenue lors de la mise à jour de la compétance.");
         }
     }
 }
