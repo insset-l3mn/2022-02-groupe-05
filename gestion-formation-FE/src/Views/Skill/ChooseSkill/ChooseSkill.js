a122e882be8a0909e140b94d4b4af8df83c57fd2
augustinde <augustin.1201@outlook.fr>
2022-03-19 19:49:33 +0100

    Migrate reactflow to v10, to fix layouting

--- gestion-formation-FE/src/Views/Skill/ChooseSkill/ChooseSkill.js
+++ gestion-formation-FE/src/Views/Skill/ChooseSkill/ChooseSkill.js
@@ -1,6 +1,5 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import axios from "axios";
-import dagre from "dagre";
+import dagre, {layout} from "dagre";
 import ReactFlow,
 {
     isNode,
@@ -9,18 +8,23 @@ import ReactFlow,
     useNodesState
 }
     from "react-flow-renderer";
+import axios from "axios";
 
 export default function ChooseSkill(){
 
     const [load, setLoad] = useState(false)
-    const [nodes, setNodes, onNodesChange] = useState([]);
-    const [edges, setEdges, onEdgesChange] = useState([]);
+    const [nodes, setNodes, onNodesChange] = useNodesState([]);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
     const dagreGraph = new dagre.graphlib.Graph();
     dagreGraph.setDefaultEdgeLabel(() => ({}));
 
     const nodeWidth = 172;
     const nodeHeight = 36;
+
+    const changeLoad = (l) => {
+        setLoad(l)
+    }
 
     const getLayoutedElements = (nodes, edges, direction = "TB") => {
         const isHorizontal = direction === "LR";
@@ -51,23 +55,21 @@ export default function ChooseSkill(){
         return { nodes, edges };
     };
 
-    useEffect(async () => {
-
-        setLoad(false)
-        await axios.get("http://localhost:8080/gestion-formation-BE/api/graph/global")
+    useEffect(() => {
+        axios.get("http://localhost:8080/gestion-formation-BE/api/graph/global")
             .then((response) => {
                 response["data"].map((item) => {
                     if (isNode(item)) {
                         let node = {}
-                        node.id = item["id"]
+                        node.id = item["id"].toString();
                         node.position = {x: 0, y: 0}
                         node.data = item["data"][0]
-                        node.dragging = false
-                        if(node.data.context === "Skill"){
-                            node.style = {...node.style, backgroundColor: "#0041D0", color:"#fff"}
-                        }else if(node.data.context === "Subdomain"){
-                            node.style = {...node.style, backgroundColor: "#FF0072", color:"#fff"}
-                        }else{
+                        // node.dragging = false
+                        if (node.data.context === "Skill") {
+                            node.style = {...node.style, backgroundColor: "#0041D0", color: "#fff"}
+                        } else if (node.data.context === "Subdomain") {
+                            node.style = {...node.style, backgroundColor: "#FF0072", color: "#fff"}
+                        } else {
                             node.style = {...node.style, backgroundColor: "#F8F9F9"}
                         }
                         setNodes(prevState => [...prevState, node])
@@ -82,16 +84,36 @@ export default function ChooseSkill(){
                     }
                 })
             })
+        onLayout('LR')
 
-        setLoad(true)
-    }, [])
+    },[])
 
     const t = () => {
+        changeLoad(true)
+
         console.log(nodes.length)
         console.log(nodes)
         console.log(edges.length)
         console.log(edges)
     }
+
+    const onLayout = useCallback(
+        (direction) => {
+            setLoad(false)
+            const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+                nodes,
+                edges,
+                direction
+            );
+
+            setNodes([...layoutedNodes]);
+            setEdges([...layoutedEdges]);
+            console.log("slt")
+            setLoad(true)
+
+        },
+        [nodes, edges]
+    );
 
     const changeNodeColor = (node, color) => {
         console.log(node, color)
@@ -130,42 +152,23 @@ export default function ChooseSkill(){
         }
     }
 
-    const onLayout = useCallback(
-        (direction) => {
-            const {
-                nodes: layoutedNodes,
-                edges: layoutedEdges
-            } = getLayoutedElements(nodes, edges, direction);
-
-            setNodes([...layoutedNodes]);
-            setEdges([...layoutedEdges]);
-        },
-        [nodes, edges]
-    );
-
     return (
         <div className="layoutflow" style={{ height: 500 }}>
-
             { load &&
-                <ReactFlowProvider>
-                    <ReactFlow
-                        nodes={nodes}
-                        edges={edges}
-                        //elements={getLayoutedElements(graph, 'LR')}
-                        nodesDraggable={false}
-                        //onElementClick={onClickElement}
-                        onNodesChange={onNodesChange}
-                        onEdgesChange={onEdgesChange}
-                        connectionLineType="smoothstep"
-                        fitView
-                    />
-                </ReactFlowProvider>
+                <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    //elements={getLayoutedElements(graph, 'LR')}
+                    //onElementClick={onClickElement}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    connectionLineType="smoothstep"
+                    fitView
+                />
             }
-            <button onClick={() => onLayout("TB")}>vertical layout</button>
-
-
 
             <button onClick={t}>click</button>
+            <button onClick={() => onLayout('LR')}>forme</button>
         </div>
     );
 
--- gestion-formation-FE/src/Views/Skill/ChooseSkill/graph.js
+++
@@ -1,4 +0,0 @@
-import {useEdgesState, useNodesState} from "react-flow-renderer";
-
-const [nodes, setNodes, onNodesChange] = useNodesState([]);
-const [edges, setEdges, onEdgesChange] = useEdgesState([]);
